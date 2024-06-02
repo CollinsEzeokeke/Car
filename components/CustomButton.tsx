5697704d07fd89e1348b627fe58aff8b5f9e7a35
Ezeokeke Collins Chidubem <90806850+CollinsEzeokeke@users.noreply.github.com>
2024-06-02 13:38:28 +0100

    Car site
    my first E-Commerce website for a car type company

---
+++ components/CustomButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const CustomButton = () => {
+  return (
+    <button disabled={false} type={'button'} className={`custom-btn`} onClick={() =>{}}>
+
+    </button>
+  )
+}
+
+export default CustomButton
